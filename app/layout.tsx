222bb7007b89cfcaac43bfaf844e7887f50a0f3d
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-03-07 20:16:57 +0500

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "AstraX | Blogs",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
+        <Analytics/>
         <Navigation/>
         {children}
         <footer>
